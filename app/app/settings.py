6c122cc7b152f0a3d47291bd07e355aedbc5639f
iqsgi <andrey-laguta@mail.ru>
2023-06-26 19:56:45 +1000

    new db and styles

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,7 +16,7 @@ import os
 
 load_dotenv()
 
-HOST = os.getenv("ALLOWED_HOSTS").split(" ")
+HOST = ['.localhost', '127.0.0.1', '[::1]']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
